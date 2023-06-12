a67d6ba195b91df0b25a780d10b459cd2c9d340b
adewale <adewalekujore6@gmail.com>
2023-06-12 18:43:20 +0100

    ui bug

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 const Page = () => {
   return (
-    <div className="w-full max-[765px]:absolute flex justify-center items-center bg-[#333333]">
+    <div className="w-full max-[765px]:absolute flex justify-center items-center bg-[#333333] min-h-screen">
       <h1 className="text-white font-bold text-2xl">Welcome to Chat App </h1>
     </div>
   );
