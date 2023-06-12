48de4f1a52f9b6006378082910ebbe0153df462f
adewale <adewalekujore6@gmail.com>
2023-06-12 19:32:34 +0100

    done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
   return (
     <div className="w-full max-[765px]:absolute flex justify-center items-center bg-[#333333] min-h-screen flex-col">
-      <h1 className="text-white font-bold text-2xl">Welcome to Chat App </h1>
+      <h1 className="text-white font-bold text-4xl">Welcome to Chat App </h1>
       <span className="text-white font-bold text-xl">
         A real-time messaging app
       </span>
